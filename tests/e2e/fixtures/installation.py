4efbe611ed7ef1a1dddd53b3a0f9573668cddaf5
jsitu <jsitu@amazon.com>
2022-08-16 16:57:38 -0700

    move install/delete kustomize/helm functions to utils.py, call deterministic install_kubeflow and uninstall_kubeflow function in fixture on create and on delete to replace the origin one line while loop command

--- tests/e2e/fixtures/installation.py
+++ tests/e2e/fixtures/installation.py
@@ -10,65 +10,10 @@ import os
 
 from e2e.utils.config import configure_resource_fixture
 from e2e.utils.constants import KUBEFLOW_VERSION
-from e2e.utils.utils import wait_for,kubectl_delete, kubectl_delete_crd
+from e2e.utils.utils import wait_for,kubectl_delete, kubectl_delete_crd, kubectl_wait_crd
+from e2e.utils.kubeflow_installation import install_kubeflow
+from e2e.utils.kubeflow_uninstallation import uninstall_kubeflow
 
-
-def apply_kustomize(path):
-    """
-    Equivalent to:
-
-    while ! kustomize build <PATH> | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 30; done
-
-    but creates a temporary file instead of piping.
-    """
-    with tempfile.NamedTemporaryFile() as tmp:
-        build_retcode = subprocess.call(f"kustomize build {path} -o {tmp.name}".split())
-        assert build_retcode == 0
-        apply_retcode = subprocess.call(f"kubectl apply -f {tmp.name}".split())
-        # to deal with runtime crds, give it another chance to build
-        if apply_retcode == 1:
-            time.sleep(30)
-            apply_retcode = subprocess.call(f"kubectl apply -f {tmp.name}".split())
-        assert apply_retcode == 0
-
-def install_helm(chart_name, path):
-    """
-    Equivalent to:
-
-    helm install <chart_name> <path>
-    
-    """
-
-    with tempfile.NamedTemporaryFile() as tmp:
-        install_retcode = subprocess.call(f"helm install {chart_name} {path}".split())
-        assert install_retcode == 0
-
-def delete_kustomize(path):
-    """
-    Equivalent to:
-
-    kustomize build <PATH> | kubectl delete -f -
-
-    but creates a temporary file instead of piping.
-    """
-    with tempfile.NamedTemporaryFile() as tmp:
-        build_retcode = subprocess.call(f"kustomize build {path} -o {tmp.name}".split())
-        assert build_retcode == 0
-    
-        delete_retcode = subprocess.call(f"kubectl delete -f {tmp.name}".split())
-
-def uninstall_helm(chart_name, namespace = None):
-    """
-    Equivalent to:
-
-    helm uninstall <chart_name>
-
-    """
-    if namespace: 
-        uninstall_retcode = subprocess.call(f"helm uninstall {chart_name} -n {namespace}".split())
-    else:
-        uninstall_retcode = subprocess.call(f"helm uninstall {chart_name}".split())
-    assert uninstall_retcode == 0
 
 
 @pytest.fixture(scope="class")
@@ -90,7 +35,7 @@ def clone_upstream():
 
 @pytest.fixture(scope="class")
 def installation(
-    metadata, cluster, clone_upstream, configure_manifests, installation_path, installation_option, request
+    metadata, aws_telemetry_option, cluster, clone_upstream, configure_manifests, installation_path, installation_option, request
 ):
     """
     This fixture is created once for each test class.
@@ -103,64 +48,15 @@ def installation(
     """
 
     def on_create():
+        print("my installation_option")
+        print(installation_option)
+        print("my aws-telemetry_option")
+        print(aws_telemetry_option)
+        install_kubeflow(installation_option, aws_telemetry_option)
 
-        if (installation_option == 'kustomize'):
-            wait_for(lambda: apply_kustomize(installation_path), timeout=20 * 60)
-            time.sleep(5 * 60)  # wait a bit for all pods to be running
-        # TODO: verify this programmatically
-        if (installation_option == 'helm'):
-
-            for helm_chart in installation_path:
-                chart_path = helm_chart[0]
-                chart_name = helm_chart[1]
-                wait_for(lambda: install_helm(chart_name, chart_path), timeout=3 * 60)
-                time.sleep(10)
-
-        time.sleep(60)
-        # wait a bit for all pods to be running
 
     def on_delete():
-         # deleting the kubeflow deployment deletes the load balancer controller at the same time as ingress
-            # the problem with this is the ingress managed load balacer does not get cleaned up as there is no controller 
-        subprocess.call(f"kubectl delete ingress -n istio-system --all".split())
-        subprocess.call(f"kubectl delete profile --all".split())
-            # load balancer controller does not place a finalizer on the ingress and so deleting the attached load balancer is asynhronous
-            # adding a random wait to allow controller to delete the load balancer
-            # TODO: implement a better check
-        time.sleep(2 * 60)
-        if (installation_option == 'kustomize'):
-            delete_kustomize(installation_path)
-            kubectl_delete_crd("authrequests.dex.coreos.com")
-            kubectl_delete_crd("connectors.dex.coreos.com")
-            kubectl_delete_crd("devicerequests.dex.coreos.com")
-            kubectl_delete_crd("devicetokens.dex.coreos.com")
-            kubectl_delete_crd("oauth2clients.dex.coreos.com")
-            kubectl_delete_crd("offlinesessionses.dex.coreos.com")
-            kubectl_delete_crd("passwords.dex.coreos.com")
-            kubectl_delete_crd("refreshtokens.dex.coreos.com")
-            kubectl_delete_crd("signingkeies.dex.coreos.com")
-
-        
-        if (installation_option == 'helm'):
-            installation_path.reverse()
-
-            for helm_chart in installation_path:
-                chart_name = helm_chart[1]
-                uninstall_helm(chart_name)
-                if os.path.isdir(f"{helm_chart[0]}/crds"):
-                    print(f"deleting {chart_name} crds ...")
-                    kubectl_delete(f"{helm_chart[0]}/crds")
-                    if chart_name == "dex":
-                        kubectl_delete_crd("authrequests.dex.coreos.com")
-                        kubectl_delete_crd("connectors.dex.coreos.com")
-                        kubectl_delete_crd("devicerequests.dex.coreos.com")
-                        kubectl_delete_crd("devicetokens.dex.coreos.com")
-                        kubectl_delete_crd("oauth2clients.dex.coreos.com")
-                        kubectl_delete_crd("offlinesessionses.dex.coreos.com")
-                        kubectl_delete_crd("passwords.dex.coreos.com")
-                        kubectl_delete_crd("refreshtokens.dex.coreos.com")
-                        kubectl_delete_crd("signingkeies.dex.coreos.com")
-
+        uninstall_kubeflow(installation_option, aws_telemetry_option)
 
 
 
