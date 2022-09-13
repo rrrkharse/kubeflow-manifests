dcbe58c3c4cb129abc46735cbf3cc121bc6ef83f
Ubuntu <ubuntu@ip-172-31-41-215.us-west-2.compute.internal>
2022-09-13 22:20:06 +0000

    fix versions

--- iaac/terraform/kubeflow-components/kubeflow-roles/versions.tf
+++ iaac/terraform/kubeflow-components/kubeflow-roles/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.2.7"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.72"
+      version = ">= 4.30.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.10"
+      version = ">= 2.13.1"
     }
   }
 }
